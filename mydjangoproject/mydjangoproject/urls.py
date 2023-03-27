d63d1abd1476e268fcb1831c7916f9cc2c65d426
k20016755 <92389367+k20016755@users.noreply.github.com>
2023-03-27 22:20:03 +0100

    Update urls.py

--- mydjangoproject/mydjangoproject/urls.py
+++ mydjangoproject/mydjangoproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from mydjangoapp.views import task_list
 
 urlpatterns = [
+    path('mydjangoapp/', include('mydjangoproject.mydjangoapp.urls', namespace='mydjangoapp')),
     path('tasks/',task_list, name='task_list'),
 ]
