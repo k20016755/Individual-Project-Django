a275f541a7c52a460bbcf9079f640d777b1e2d7e
k20016755 <rafay.ahmed@kcl.ac.uk>
2023-03-27 23:41:56 +0100

    bye bye

--- mydjangoproject/mydjangoapp/views.py
+++ mydjangoproject/mydjangoapp/views.py
@@ -4,5 +4,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def task_list(request):
     tasks = Task.objects.filter(assigned_to=request.user)
-    return render(request, 'mydjangoapp/task_list.html', {'tasks': tasks})
+    context ={'tasks': tasks}
+    return render(request, 'tasks/task_list.html', context)
 # Create your views here.
