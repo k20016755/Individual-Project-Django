472711c4a8fa293f2855befca27518f98c99867d
k20016755 <92389367+k20016755@users.noreply.github.com>
2023-03-27 21:38:15 +0100

    Update test_views.py

--- mydjangoproject/mydjangoapp/test_views.py
+++ mydjangoproject/mydjangoapp/test_views.py
@@ -6,14 +6,11 @@ class TESTVIEWS(TestCase):
     @pytest.mark.django_db
     def test_task_list_view(self):
         factory = RequestFactory()
-        self.assertTemplateUsed('mydjangoapp/task_list.html')
         request = factory.get(reverse('task_list'))
         response = task_list(request)
+        self.assertTemplateUsed('mydjangoapp/task_list.html')
         self.assertContains(response,'<h1>Task List<h1>')
-    
-
        
-
         assert response.status_code == 200
     
 
