931772869de038667e53049aaacc85d68d671388
k20016755 <rafay.ahmed@kcl.ac.uk>
2023-03-27 21:43:45 +0100

    please work

--- mydjangoproject/mydjangoapp/test_views.py
+++ mydjangoproject/mydjangoapp/test_views.py
@@ -2,14 +2,31 @@ import pytest
 from django.urls import reverse
 from django.test import TestCase,RequestFactory
 from mydjangoapp.views import task_list
+from datetime import date
+from django.utils import timezone
+from mydjangoapp.models import Task
 class TESTVIEWS(TestCase):
+    def setUp(self):
+        self.task1 = Task.objects.create(
+            title="Task 1",
+            description="Description 1",
+            completed=False,
+            created=timezone.now(),
+            due=date.today()
+        )
+        self.task2 = Task.objects.create(
+            title="Task 2",
+            description="Description 2",
+            completed=True,
+            created=timezone.now(),
+            due=date.today()
+        )
     @pytest.mark.django_db
     def test_task_list_view(self):
-        factory = RequestFactory()
-        self.assertTemplateUsed('mydjangoapp/task_list.html')
-        request = factory.get(reverse('task_list'))
-        response = task_list(request)
-        self.assertContains(response,'<h1>Task List<h1>')
+        response = self.client.get(reverse('mydjangoapp:task_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.task1.title)
+        self.assertContains(response, self.task2.title)
     
 
        
