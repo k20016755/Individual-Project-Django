07804ffca97b7bcb8a185cfb66a713fedda688ef
k20016755 <92389367+k20016755@users.noreply.github.com>
2023-03-27 18:46:06 +0100

    Update views.py

--- mydjangoproject/mydjangoapp/views.py
+++ mydjangoproject/mydjangoapp/views.py
@@ -3,5 +3,5 @@ from .models import Task
 
 def task_list(request):
     tasks = Task.objects.all()
-    return render(request, 'mydjangoapp/task_list.html', {'tasks': tasks})
+    return render(request, 'mydjangoproject/mydjangoapp/templates/task_list.html', {'tasks': tasks})
 # Create your views here.
