60172d5a20ce0916fa70b8bb7a489aa388677143
k20016755 <rafay.ahmed@kcl.ac.uk>
2023-03-27 21:48:44 +0100

    datefield change

--- mydjangoproject/mydjangoapp/models.py
+++ mydjangoproject/mydjangoapp/models.py
@@ -3,8 +3,8 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
